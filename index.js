e912c832be2e4119719d57a2e72f5ae0c5a832d9
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2024-10-24 09:41:13 +0530

    SPRIN-139 (task) Fisrt epic

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mysql = require('mysql');
 const { exec } = require('child_process');
+const Notification = require("../../models/Notification");
+const SocketMapping = require("../../models/SocketMapping");
+const { dropRight } = require("lodash");
 
 const app = express();
 const port = 3000;
@@ -46,3 +49,41 @@ app.get('/random', (req, res) => {
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
+
+module.exports = async(io, adminId, notificationObj) => {
+    //notify to the admin through socket.io
+    //first save notification
+    let notificationObjOfAdmin = await Notification.findOne({ admin: adminId });
+    if (!notificationObjOfAdmin) {
+        // create new notification
+        notificationObjOfAdmin = new Notification({ admin: adminId, notifications: [notificationObj], noOfUnseen: 1 });
+        await notificationObjOfAdmin.save();
+    } else {
+        let notifications = notificationObjOfAdmin.notifications;
+        notifications.unshift(notificationObj);
+        notificationObjOfAdmin.noOfUnseen += 1;
+        if (notificationObjOfAdmin.noOfUnseen < 20 && notifications.length > 20) {
+            notificationObjOfAdmin.notifications = dropRight(notifications, notifications.length - 20);
+        }
+        await notificationObjOfAdmin.save();
+    }
+    //now notifying to the admin
+    let socketUser = await SocketMapping.find({ user: adminId });
+    if (socketUser.length) {
+        //for every same login user emit notification
+        socketUser.forEach(u => {
+            io.to(u.socketId).emit('notification', { noOfUnseen: notificationObjOfAdmin.noOfUnseen });
+        });
+    }
+};
+
+module.exports = () => {
+    const self = module.exports;
+    mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false })
+        .then(() => console.log("DB Connected"))
+        .catch(err => {
+            console.error("Failed to connect to the database on startup - retrying in 5 sec");
+            setTimeout(self, 5000);
+        });
+    return Fawn.init(mongoose, process.env.TRANS_COLL);
+};
